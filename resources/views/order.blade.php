c9746ab18460f29d7dae52d92a78db1a5292825a
Iago Silva da Trindade <iago23st1@gmail.com>
2023-08-31 18:36:05 -0300

    Atualização no layout da página Order

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -23,69 +23,163 @@
             <x-default_primary_button tag="a" text="Reembolso" type=""></x-default_primary_button>
         </div>
 
-        <div class="order-top-info">
-            <div class="order-main-info-area">
-                <div class="order-main-info-item">
-                    <div class="product-item-area">
-                        <div class="stock-info-area">
-                            <h4>Estoque</h4>
-                            <p>19 Produtos</p>
+        <div class="order-product-item-area">
+            <div class="stock-info-area default-flex-column">
+                <h4>Estoque:</h4>
+                <p>19 Produtos</p>
+            </div>
 
-                            <div class="product-img-area default-flex-start">
-                                <img src="assets/images/products_images/camisa.png" alt="Imagem do Produto">
+            <div class="product-info-area default-flex-between">
+                <div class="product-info-left-area default-flex">
+                    <img src="assets/images/products_images/camisa.png" alt="Imagem do Produto">
 
-                                <div class="product-info-area default-flex-column">
-                                    <a href="">Blusa Preta Feminina</a>
+                    <div class="product-info-text">
+                        <a href="">Blusa Preta Feminina</a>
 
-                                    <p>Código XX0000</p>
-                                </div>
-
-                                <div class="product-info-right default-flex-end">
-                                    <p class="product-price">39,90</p>
-                                    <p>79,90</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="action-buttons-area default-flex-end">
-                        <x-default_secondary_button id="" text="Sem Estoque"></x-default_secondary_button>
-                        <x-default_primary_button tag="a" text="Processar Pedido" type=""></x-default_primary_button>
+                        <p>Código: 0144458</p>
                     </div>
                 </div>
 
-                <div class="payment-info-area">
-                    <div class="default-flex-between">
-                        <p class="payment-info-bold">Subtotal</p>
-                        <p class="payment-info-bold">2 Itens</p>
-                        <p class="payment-info-normal default-flex-end">R$ 79,90</p>
-                    </div>
+                <div class="product-info-right-area default-flex">
+                    <p class="product-info-prices">R$ 39,90 X2</p>
+                    <p class="product-info-total-amount">R$ 79,90</p>
+                </div>
+            </div>
 
-                    <div class="default-flex-between">
-                        <p class="payment-info-bold">Frete</p>
-                        <p class="payment-info-bold" >Frete Grátis (5.0Kg)</p>
-                        <p class="payment-info-normal default-flex-end">R$ 00,00</p>
-                    </div>
+            <div class="order-product-actions-area default-flex-end">
+                <x-default_secondary_button id="" text="Sem Estoque"></x-default_secondary_button>
+                <x-default_primary_button tag="a" type="" text="Processar Pedido"></x-default_primary_button>
+            </div>
+        </div>
 
-                    <div class="default-flex-between">
-                        <p class="payment-info-bold">Total</p>
-                        <p class="payment-info-bold default-flex-end">R$ 79,90</p>
-                    </div>
+        <div class="order-products-values-area">
+            <div class="subtotal-amount-area default-flex-between">
+                <p>Subtotal</p>
+                <p>2 Itens</p>
+                <p class="default-flex-end">R$ 79,90</p>
+            </div>
 
-                    <div class="paid-by-client-area default-flex-between">
-                        <p class="payment-info-normal">Pago pelo Cliente</p>
-                        <p class="payment-info-normal default-flex-end">R$ 79,90</p>
-                    </div>
+            <div class="shipping-amount-area default-flex-between">
+                <p>Frete</p>
+                <p>Frete Grátis (5.0Kg)</p>
+                <p class="default-flex-end">R$ 00,00</p>
+            </div>
 
-                    <div class="action-buttons-area default-flex-end">
-                        <x-default_secondary_button id="" text="Enviar Fatura"></x-default_secondary_button>
-                        <x-default_primary_button tag="a" text="Confirmar Pagamento" type=""></x-default_primary_button>
+            <div class="total-amount-area default-flex-between">
+                <p>Total</p>
+                <p class="default-flex-end">R$ 79,90</p>
+            </div>
+
+            <div class="payment-type-area default-flex-between">
+                <p>Pago pelo Cliente</p>
+                <p class="default-flex-end">R$ 79,90</p>
+            </div>
+
+            <div class="order-payment-product-actions-area default-flex-end">
+                <x-default_secondary_button id="" text="Enviar Fatura"></x-default_secondary_button>
+                <x-default_primary_button tag="a" type="" text="Confirmar Pagamento"></x-default_primary_button>
+            </div>
+        </div>
+
+        <div class="timeline-area">
+            <h3>Cronograma</h3>
+
+            <div class="default-flex">
+                <div class="timeline-item default-flex-column-start">
+                    <p>E-mail de confirmação enviado</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                    <a href="">Enviar E-mail</a>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pagamento feito com sucesso</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pedido em processamento</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pedido Processado</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pedido Enviado a transportadora</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pedido em rota de entrega</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
+                    </div>
+                    <a href="">Verificar andamento</a>
+                </div>
+
+                <div class="timeline-item default-flex-column-start">
+                    <p>Pedido entregue ao cliente</p>
+                    <div class="time-line-event default-flex">
+                        <div class="timeline-line-left"></div>
+                        <i class='bx bxs-circle'></i>
+                        <div class="timeline-line-right"></div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
+        <div class="observation-area">
+            <h4>Observações</h4>
+            <p>Não á observações do cliente.</p>
+        </div>
 
+        <div class="client-info-area">
+            <div class="basic-info">
+                <h4>Cliente</h4>
+                <p>Iago Silva</p>
+                <a href="">Ver Perfil</a>
+            </div>
+            
+            <div class="contact-info">
+                <h4>Informações de Contato</h4>
+                <a href="">iago23st1@gmail.com</a>
+                <p>51991657516  </p>
+            </div>
+
+            <div class="shipping-info">
+                <h4>Endereço de Entrega</h4>
+                <p>Iago Silva</p>
+                <p>Rua San Felipe 66 - Stella Maris</p>
+                <p>Cep: 33333-222</p>
+                <p>Casa</p>
+                <p>Referencia: Em frente a um campo</p>
+
+                <a href="">Ver no Mapa</a>
+            </div>
+        </div>
 </x-adm_layout>
 
