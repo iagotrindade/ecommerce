868de6519540f04318136be37e7ccab02bf7a064
3º SGT Iago Silva <3rm-iagosilva@qgcms.local>
2024-03-19 16:16:45 -0300

    Dando continuidade ao Layout Mobile do Perfil

--- app/Livewire/MobileProfileArea.php
+++ app/Livewire/MobileProfileArea.php
@@ -9,6 +9,9 @@ class MobileProfileArea extends Component
 {   
     public $user;
     public $profileDisplay = "none";
+    public $myDataDisplay = "none";
+    public $myOrdersDisplay = "none";
+    public $myFavoritesDisplay = "none";
 
     public function render()
     {
@@ -29,5 +32,38 @@ class MobileProfileArea extends Component
     public function mount($user)
     {
         $this->user = $user;
+    }
+
+    public function showMyDataTab() {
+        if($this->myDataDisplay == "block") {
+            $this->myDataDisplay = "none";
+        } else {
+            $this->myDataDisplay = "block";
+        }
+
+        $this->myOrdersDisplay = "none";
+        $this->myFavoritesDisplay = "none";
+    }
+
+    public function showMyOrdersTab() {
+        if($this->myOrdersDisplay == "block") {
+            $this->myOrdersDisplay = "none";
+        } else {
+            $this->myOrdersDisplay = "block";
+        }
+
+        $this->myDataDisplay = "none";
+        $this->myFavoritesDisplay = "none";
+    }
+
+    public function showMyFavoritesTab() {
+        if($this->myFavoritesDisplay == "block") {
+            $this->myFavoritesDisplay = "none";
+        } else {
+            $this->myFavoritesDisplay = "block";
+        }
+
+        $this->myDataDisplay = "none";
+        $this->myOrdersDisplay = "none";
     }
 }
--- resources/views/livewire/mobile-profile-area.blade.php
+++ resources/views/livewire/mobile-profile-area.blade.php
@@ -10,30 +10,128 @@
 
         <div class="profile-menu-area">
             <ul class="profile-menu-area-list">
-                <li>
-                    <p class="profile-menu-item">
+                <li class="profile-menu-item" wire:click="showMyDataTab()">
+                    <p>
                         Meus dados
                     </p>
                 </li>
 
-                <li>
-                    <p class="profile-menu-item">
+                <li class="profile-menu-item" wire:click="showMyOrdersTab()">
+                    <p>
                         Meus Pedidos
                     </p>
                 </li>
 
-                <li>
-                    <p class="profile-menu-item">
+                <li class="profile-menu-item" wire:click="showMyFavoritesTab()">
+                    <p>
                         Meus Favoritos
                     </p>
                 </li>
 
-                <li>
-                    <a href="{{route('logout', $user->id)}}">
+                <li class="profile-menu-item mobile-logout-button-area">
+                    <a href="{{route('logout', $user->id)}}" class="mobile-logout-button">
                         Sair
                     </a>
                 </li>
             </ul>
+        </div>
+
+        <div class="mobile-profile-my-data-tab" style="display: {{$myDataDisplay}};">
+            <div class="mobile-cart-header default-flex-between">
+                <a class="default-flex" wire:click="showMyDataTab()">
+                    <i class='bx bx-chevron-left'></i>
+                </a>
+    
+                <h3>{{$user->name}}</h3>
+            </div>
+
+            <div class="mobile-edit-user-data">
+                <form action="">
+                    <h4>Informações Pessoais</h4>
+
+                    <div class="mobile-user-data">
+                        <div class="cart-shipping-area-input-group default-flex-around">
+                            <div class="shipping-input-item default-flex-column w-100">
+                                <label for="name">Nome</label>
+                                <input class="w-100" type="text" name="name" id="name" value="{{$user->name}}">
+                            </div>
+        
+                            <div class="shipping-input-item default-flex-column w-100">
+                                <label for="user">Usuário</label>
+                                <input class="w-100" type="text" name="user" id="user" value="{{$user->username}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <h4>Dados de Contato</h4>
+
+                    <div class="mobile-user-data">
+                        <div class="cart-shipping-area-input-group default-flex-around">
+                            <div class="shipping-input-item default-flex-column w-100">
+                                <label for="email">E-Mail</label>
+                                <input class="w-100" type="email" name="email" id="email" value="{{$user->email}}">
+                            </div>
+        
+                            <div class="shipping-input-item default-flex-column w-100">
+                                <label for="phone">Whatsapp</label>
+                                <input class="w-100" type="text" name="phone" id="phone" value="{{$user->phone}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="default-flex-end">
+                        <button class="update-user-data" type="submit">Salvar</button>
+                    </div>
+                </form>
+
+                <h4>Endereços</h4>
+
+                <div class="add-new-address-button default-flex-between">
+                    <div class="add-new-address-left-area default-flex">
+                        <i class='bx bxs-map-pin'></i>
+                        <p>Cadastrar Novo Endereço</p>
+                    </div>
+
+                    <div class="add-new-address-right-area default-flex">
+                        <i class='bx bxs-plus-circle'></i>
+                    </div>
+                </div>
+
+                <div class="user-addresses default-flex-between">
+                    <div class="add-new-address-left-area default-flex">
+                        <i class='bx bxs-map-pin'></i>
+                        <div class="user-address-area default-flex-column">
+                            <p class="user-compiled-address">R. Cândido de Souza, 952</p>
+                            <p class="user-address-complement">Casa - Frente ao Deposito de Gas</p>
+                        </div>
+                    </div>
+
+                    
+                    <div class="add-new-address-right-area default-flex">
+                        <i class='bx bx-current-location'></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mobile-profile-my-orders-tab" style="display: {{$myOrdersDisplay}};">
+            <div class="mobile-cart-header default-flex-between">
+                <a class="default-flex" wire:click="showMyOrdersTab()">
+                    <i class='bx bx-chevron-left'></i>
+                </a>
+    
+                <h3>{{$user->name}}</h3>
+            </div>
+        </div>
+
+        <div class="mobile-profile-my-favorites-tab" style="display: {{$myFavoritesDisplay}};">
+            <div class="mobile-cart-header default-flex-between">
+                <a class="default-flex" wire:click="showMyFavoritesTab()">
+                    <i class='bx bx-chevron-left'></i>
+                </a>
+    
+                <h3>{{$user->name}}</h3>
+            </div>
         </div>
     </div>
 </div>
