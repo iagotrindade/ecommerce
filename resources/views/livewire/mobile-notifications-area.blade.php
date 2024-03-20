4fcdf63a73a202361fd362c2a26e4eb564a42b6c
Iago Silva <iagost1@hotmail.com>
2024-03-19 22:25:52 -0300

    Corrigindo bug no site ao entrar sem conta nas notificações e perfil

--- resources/views/livewire/mobile-notifications-area.blade.php
+++ resources/views/livewire/mobile-notifications-area.blade.php
@@ -8,40 +8,44 @@
             <h3>Notificações</h3>
         </div>
 
-        <div class="mobile-select-notification-type default-flex-between">
-            <div class="mobile-notifications" wire:click="changeNotificationsTab('notifications')">
-                <p class="@if ($mobileNotifications == 'block')
-                    mobile-notifications-active
-                @endif">Minhas Atividades</p>
-            </div>
-
-            <div class="mobile-news" wire:click="changeNotificationsTab('news')">
-                <p class="@if ($mobileNews == 'block')
-                    mobile-notifications-active
-                @endif">Novidades</p>
-            </div>
-        </div>
-
-        <div class="my-notifications-area" style="display: {{$mobileNotifications}}">
-            <h3>Janeiro, 2024</h3>
-
-            @foreach ($user->notifications as $notification)
-                <div class="mobile-notification-item default-flex-column" @if($notification->read_at !== null) style="opacity: 0.5;" @endif>
-                    <div class="mobile-notification-content default-flex-between">
-                        <h5>{{$notification->data['title']}}</h5>
-
-                        <p class="mobile-notification-date">
-                            {{date('d/m/Y - h:m', strtotime($notification->created_at))}}
-                        </p>
-                    </div>
-
-                    <p>{{Str::limit($notification->data['body'], 70, '...')}}</p>
+        @if(Auth::check())
+            <div class="mobile-select-notification-type default-flex-between">
+                <div class="mobile-notifications" wire:click="changeNotificationsTab('notifications')">
+                    <p class="@if ($mobileNotifications == 'block')
+                        mobile-notifications-active
+                    @endif">Minhas Atividades</p>
                 </div>
-            @endforeach
-        </div>
 
-        <div class="news-area" style="display: {{$mobileNews}}">
-            <h3>Janeiro, 2024</h3>
-        </div>
+                <div class="mobile-news" wire:click="changeNotificationsTab('news')">
+                    <p class="@if ($mobileNews == 'block')
+                        mobile-notifications-active
+                    @endif">Novidades</p>
+                </div>
+            </div>
+
+            <div class="my-notifications-area" style="display: {{$mobileNotifications}}">
+                <h3>Janeiro, 2024</h3>
+
+                @foreach ($user->notifications as $notification)
+                    <div class="mobile-notification-item default-flex-column" @if($notification->read_at !== null) style="opacity: 0.5;" @endif>
+                        <div class="mobile-notification-content default-flex-between">
+                            <h5>{{$notification->data['title']}}</h5>
+
+                            <p class="mobile-notification-date">
+                                {{date('d/m/Y - h:m', strtotime($notification->created_at))}}
+                            </p>
+                        </div>
+
+                        <p>{{Str::limit($notification->data['body'], 70, '...')}}</p>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="news-area" style="display: {{$mobileNews}}">
+                <h3>Janeiro, 2024</h3>
+            </div>
+        @else
+            <h4>É preciso estar logado para ver suas Notificações</h4>
+        @endif
     </div>
 </div>
--- resources/views/livewire/mobile-profile-area.blade.php
+++ resources/views/livewire/mobile-profile-area.blade.php
@@ -5,133 +5,141 @@
                 <i class='bx bx-chevron-left'></i>
             </a>
 
-            <h3>{{$user->name}}</h3>
+            @if(Auth::check())
+                <h3>{{$user->name}}</h3>
+            @else
+                <h3>Perfil</h3>
+            @endif
         </div>
 
-        <div class="profile-menu-area">
-            <ul class="profile-menu-area-list">
-                <li class="profile-menu-item" wire:click="showMyDataTab()">
-                    <p>
-                        Meus dados
-                    </p>
-                </li>
+        @if(Auth::check())
+            <div class="profile-menu-area">
+                <ul class="profile-menu-area-list">
+                    <li class="profile-menu-item" wire:click="showMyDataTab()">
+                        <p>
+                            Meus dados
+                        </p>
+                    </li>
 
-                <li class="profile-menu-item" wire:click="showMyOrdersTab()">
-                    <p>
-                        Meus Pedidos
-                    </p>
-                </li>
+                    <li class="profile-menu-item" wire:click="showMyOrdersTab()">
+                        <p>
+                            Meus Pedidos
+                        </p>
+                    </li>
 
-                <li class="profile-menu-item" wire:click="showMyFavoritesTab()">
-                    <p>
-                        Meus Favoritos
-                    </p>
-                </li>
+                    <li class="profile-menu-item" wire:click="showMyFavoritesTab()">
+                        <p>
+                            Meus Favoritos
+                        </p>
+                    </li>
 
-                <li class="profile-menu-item mobile-logout-button-area">
-                    <a href="{{route('logout', $user->id)}}" class="mobile-logout-button">
-                        Sair
+                    <li class="profile-menu-item mobile-logout-button-area">
+                        <a href="{{route('logout', $user->id)}}" class="mobile-logout-button">
+                            Sair
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mobile-profile-my-data-tab" style="display: {{$myDataDisplay}};">
+                <div class="mobile-cart-header default-flex-between">
+                    <a class="default-flex" wire:click="showMyDataTab()">
+                        <i class='bx bx-chevron-left'></i>
                     </a>
-                </li>
-            </ul>
-        </div>
 
-        <div class="mobile-profile-my-data-tab" style="display: {{$myDataDisplay}};">
-            <div class="mobile-cart-header default-flex-between">
-                <a class="default-flex" wire:click="showMyDataTab()">
-                    <i class='bx bx-chevron-left'></i>
-                </a>
-    
-                <h3>{{$user->name}}</h3>
-            </div>
-
-            <div class="mobile-edit-user-data">
-                <form action="">
-                    <h4>Informações Pessoais</h4>
-
-                    <div class="mobile-user-data">
-                        <div class="cart-shipping-area-input-group default-flex-around">
-                            <div class="shipping-input-item default-flex-column w-100">
-                                <label for="name">Nome</label>
-                                <input class="w-100" type="text" name="name" id="name" value="{{$user->name}}">
-                            </div>
-        
-                            <div class="shipping-input-item default-flex-column w-100">
-                                <label for="user">Usuário</label>
-                                <input class="w-100" type="text" name="user" id="user" value="{{$user->username}}">
-                            </div>
-                        </div>
-                    </div>
-
-                    <h4>Dados de Contato</h4>
-
-                    <div class="mobile-user-data">
-                        <div class="cart-shipping-area-input-group default-flex-around">
-                            <div class="shipping-input-item default-flex-column w-100">
-                                <label for="email">E-Mail</label>
-                                <input class="w-100" type="email" name="email" id="email" value="{{$user->email}}">
-                            </div>
-        
-                            <div class="shipping-input-item default-flex-column w-100">
-                                <label for="phone">Whatsapp</label>
-                                <input class="w-100" type="text" name="phone" id="phone" value="{{$user->phone}}">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="default-flex-end">
-                        <button class="update-user-data" type="submit">Salvar</button>
-                    </div>
-                </form>
-
-                <h4>Endereços</h4>
-
-                <div class="add-new-address-button default-flex-between">
-                    <div class="add-new-address-left-area default-flex">
-                        <i class='bx bxs-map-pin'></i>
-                        <p>Cadastrar Novo Endereço</p>
-                    </div>
-
-                    <div class="add-new-address-right-area default-flex">
-                        <i class='bx bxs-plus-circle'></i>
-                    </div>
+                    <h3>{{$user->name}}</h3>
                 </div>
 
-                <div class="user-addresses default-flex-between">
-                    <div class="add-new-address-left-area default-flex">
-                        <i class='bx bxs-map-pin'></i>
-                        <div class="user-address-area default-flex-column">
-                            <p class="user-compiled-address">R. Cândido de Souza, 952</p>
-                            <p class="user-address-complement">Casa - Frente ao Deposito de Gas</p>
+                <div class="mobile-edit-user-data">
+                    <form action="">
+                        <h4>Informações Pessoais</h4>
+
+                        <div class="mobile-user-data">
+                            <div class="cart-shipping-area-input-group default-flex-around">
+                                <div class="shipping-input-item default-flex-column w-100">
+                                    <label for="name">Nome</label>
+                                    <input class="w-100" type="text" name="name" id="name" value="{{$user->name}}">
+                                </div>
+
+                                <div class="shipping-input-item default-flex-column w-100">
+                                    <label for="user">Usuário</label>
+                                    <input class="w-100" type="text" name="user" id="user" value="{{$user->username}}">
+                                </div>
+                            </div>
+                        </div>
+
+                        <h4>Dados de Contato</h4>
+
+                        <div class="mobile-user-data">
+                            <div class="cart-shipping-area-input-group default-flex-around">
+                                <div class="shipping-input-item default-flex-column w-100">
+                                    <label for="email">E-Mail</label>
+                                    <input class="w-100" type="email" name="email" id="email" value="{{$user->email}}">
+                                </div>
+
+                                <div class="shipping-input-item default-flex-column w-100">
+                                    <label for="phone">Whatsapp</label>
+                                    <input class="w-100" type="text" name="phone" id="phone" value="{{$user->phone}}">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="default-flex-end">
+                            <button class="update-user-data" type="submit">Salvar</button>
+                        </div>
+                    </form>
+
+                    <h4>Endereços</h4>
+
+                    <div class="add-new-address-button default-flex-between">
+                        <div class="add-new-address-left-area default-flex">
+                            <i class='bx bxs-map-pin'></i>
+                            <p>Cadastrar Novo Endereço</p>
+                        </div>
+
+                        <div class="add-new-address-right-area default-flex">
+                            <i class='bx bxs-plus-circle'></i>
                         </div>
                     </div>
 
-                    
-                    <div class="add-new-address-right-area default-flex">
-                        <i class='bx bx-current-location'></i>
+                    <div class="user-addresses default-flex-between">
+                        <div class="add-new-address-left-area default-flex">
+                            <i class='bx bxs-map-pin'></i>
+                            <div class="user-address-area default-flex-column">
+                                <p class="user-compiled-address">R. Cândido de Souza, 952</p>
+                                <p class="user-address-complement">Casa - Frente ao Deposito de Gas</p>
+                            </div>
+                        </div>
+
+
+                        <div class="add-new-address-right-area default-flex">
+                            <i class='bx bx-current-location'></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="mobile-profile-my-orders-tab" style="display: {{$myOrdersDisplay}};">
-            <div class="mobile-cart-header default-flex-between">
-                <a class="default-flex" wire:click="showMyOrdersTab()">
-                    <i class='bx bx-chevron-left'></i>
-                </a>
-    
-                <h3>{{$user->name}}</h3>
-            </div>
-        </div>
+            <div class="mobile-profile-my-orders-tab" style="display: {{$myOrdersDisplay}};">
+                <div class="mobile-cart-header default-flex-between">
+                    <a class="default-flex" wire:click="showMyOrdersTab()">
+                        <i class='bx bx-chevron-left'></i>
+                    </a>
 
-        <div class="mobile-profile-my-favorites-tab" style="display: {{$myFavoritesDisplay}};">
-            <div class="mobile-cart-header default-flex-between">
-                <a class="default-flex" wire:click="showMyFavoritesTab()">
-                    <i class='bx bx-chevron-left'></i>
-                </a>
-    
-                <h3>{{$user->name}}</h3>
+                    <h3>{{$user->name}}</h3>
+                </div>
             </div>
-        </div>
+
+            <div class="mobile-profile-my-favorites-tab" style="display: {{$myFavoritesDisplay}};">
+                <div class="mobile-cart-header default-flex-between">
+                    <a class="default-flex" wire:click="showMyFavoritesTab()">
+                        <i class='bx bx-chevron-left'></i>
+                    </a>
+
+                    <h3>{{$user->name}}</h3>
+                </div>
+            </div>
+        @else
+            <h4>É preciso estar logado para ver seus Perfil</h4>
+        @endif
     </div>
 </div>
